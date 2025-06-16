6b7f0d7f2182287af230f9d0ce88efe8c5350154
Moody-0x01 <AzmoudHossin0101@gmail.com>
2025-06-16 14:01:03 +0100

    Added a mechanism to keep it from running or doing anything while the computer is being charged.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - Only [dunst](https://github.com/dunst-project/dunst "Lightweight and customizable notification daemon")
 
 # Set up + Usage
-- Install dunst with pacman, dnf, or apt-get, then get it started with this command `dunst & disown`
+- Install dunst with pacman, dnf, orapt-get, then get it started with this command `dunst & disown`
 - run `./run.sh` which will compile the binary and run it as an independent process
 - build `./build.sh` to just build the binary and place it in the `./build` folder.
 - kill `./kill.sh` kills the watcher.
--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 #define PATH_COUNT 3
+#define N 1024
+const char* status_path = "/sys/class/power_supply/BAT0/status";
 const char* battery_paths[PATH_COUNT] = {
 	"/sys/class/power_supply/BAT0/capacity",
 	"/sys/class/power_supply/BAT1/capacity",
 	"/sys/class/power_supply/battery/capacity"
 };
 
-void get_battery_perc(int fd, char buffer[1024])
+void buff_read(int fd, char buffer[N])
 {
 	int n = 0;
 	while(1)
@@ -18,23 +21,55 @@ void get_battery_perc(int fd, char buffer[1024])
 		if (read(fd, buffer + n, 1) == 0)
 			break ;
 		n++;
-		if (n >= 1024)
+		if (n >= N)
 			break ;
 	}
 }
 
+int is_battery_chargin(void)
+{
+	char buffer[N] = {0};
+	int status_fd = open(status_path, O_RDONLY);
+
+	if (status_fd == -1)
+		return (-1);
+	buff_read(status_fd, buffer);
+	if (strcmp(buffer, "Charging") == 0)
+		return (1);
+	return (0);
+}
+
+void get_battery_perc(int fd, char buffer[N])
+{
+	buff_read(fd, buffer);
+}
+
 void monitor_battery(int bat)
 {
-	char buffer[1024 * 2] = {0};
+	char buffer[N * 2] = {0};
+	int status;
 	int perc;
+
 	while (1)
 	{
 		lseek(bat, 0, SEEK_SET);
+		status = is_battery_chargin();
+		if (status)
+		{
+			sleep(15);
+			continue;
+		}
+		if (status == -1)
+		{
+			fprintf(stderr, "%s: %s: ", "watcher", "Failed to get the status of the battery");
+			perror("");
+			break ;
+		}
 		get_battery_perc(bat, buffer);
 		perc = atoi(buffer);
 		if (perc <= 15)
 		{
-			snprintf(buffer, (1024 * 2), "notify-send -u critical \"dawg, I\'m running out of power. plug me in [perc: %i]\"\n", perc);
+			snprintf(buffer, (N * 2), "notify-send -u critical \"dawg, I\'m running out of power. plug me in [perc: %i]\"\n", perc);
 			int status = system(buffer);
 			if (status != 0)
 				return ;
@@ -62,5 +97,6 @@ int main(int ac, char **av)
 		return (1);
 	}
 	monitor_battery(bat0);
+	close(bat0);
 	return (0);
 }
